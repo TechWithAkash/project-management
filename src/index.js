18107112d6f77365570e288a8cecb3aa4fb779b7
TechWithAkash <vishwakarmaakashav17@gmail.com>
2026-01-03 20:46:39 +0530

    MongoDB Configuration done

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import dotenv from "dotenv";
 import app from "./app.js"
+import connectDB from "./db/index.js";
 dotenv.config({
     path: "./.env",
 });
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-    console.log(`Express App listening on Port http://localhost:${port}`)
-})
+connectDB()
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`Express App listening on Port http://localhost:${port}`)
+        })
+    })
+    .catch((error) => {
+        console.error("MongoDB Connectino Error".error)
+    })
