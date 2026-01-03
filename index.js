e38a83d6013d5f6e51548bf27fe3a5bee6ec51ac
TechWithAkash <vishwakarmaakashav17@gmail.com>
2026-01-03 20:30:39 +0530

    Express server, API Handling and Constants done

--- index.js
+++
@@ -1,1 +0,0 @@
-console.log("Start of the Backend Server");
---
+++ src/utils/constants.js
@@ -0,0 +1,14 @@
+export const userRolesEnum = {
+    ADMIN: "admin",
+    PROJECT_ADMIN: "project_admin",
+    MEMBER: "member"
+}
+export const AvailableUserRole = Object.values(userRolesEnum)
+
+export const TaskStatusEnum = {
+    TODO: "todo",
+    IN_PROGRESS: "in_progress",
+    DONE: "done"
+}
+
+export const AvailableTaskStatus = object.values(TaskStatusEnum)
